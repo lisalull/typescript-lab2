c83a9725d8d359a16532ee3da4ea4be639a9c54a
lisa <lisa.marie.lull@gmail.com>
2021-10-25 09:20:56 -0400

    added index file

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Greeter } from "./models/Greeter";
+import { HTMLGreeter } from "./models/HTMLGreeter";
+import { JavaScriptGreeter } from "./models/JavaScriptGreeter";
+import { LoudGreeter } from "./models/LoudGreeter";
+
+console.log(Greeter);
+console.log(LoudGreeter);
+console.log(JavaScriptGreeter);
+console.log(HTMLGreeter);
--- src/models/HTMLGreeter.ts
+++ src/models/HTMLGreeter.ts
@@ -1,12 +1,14 @@
 import { Greeter } from "./Greeter";
 
 export class HTMLGreeter extends Greeter {
-  tagName: string = "h1";
-  constructor(greeting: string, tagName?: string) {
+  // tagName: string = "h1";
+  tagName: string;
+  // constructor(greeting: string, tagName?: string) {
+  constructor(greeting: string, tagName: string = "h1") {
     super(greeting);
-    if (typeof tagName !== "undefined") {
-      this.tagName = tagName;
-    }
+    // if (typeof tagName !== "undefined") {
+    this.tagName = tagName;
+    // }
   }
   greet(name: string): string {
     return `<${this.tagName}>${this.greeting}, ${name}!</${this.tagName}>`;
